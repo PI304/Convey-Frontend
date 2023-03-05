9e44247c4158cae9fbdf2a8ac4c39c1f706438f0
sookyeongyeom <sookyeong.dev@gmail.com>
2023-03-06 06:14:30 +0900

    Feat: Survey model

--- hooks/useInput.tsx
+++ hooks/useInput.tsx
@@ -2,12 +2,7 @@ import { useState } from 'react';
 
 export const useInput = (
   initialValue = '',
-): [
-  string,
-  (e: React.ChangeEvent<HTMLInputElement>) => void,
-  () => void,
-  (value: string) => void,
-] => {
+): [string, (e: React.ChangeEvent<HTMLInputElement>) => void, () => void, (value: string) => void] => {
   const [value, setValue] = useState(initialValue);
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
   const onResetValue = () => setValue(initialValue);
---
+++ models/survey.ts
@@ -0,0 +1,143 @@
+import { immerable } from 'immer';
+import { QuestionTypes } from '../constants/questionTypes';
+
+export class Survey implements SurveyType {
+  [immerable] = true;
+
+  title = '제목';
+  description = '설명';
+  questionType = '';
+  commonChoices: ChoiceType[] | null = null;
+  questions: QuestionType[] = [
+    {
+      number: 1,
+      content: '질문1',
+      isRequired: true,
+      linkedSector: null,
+      choices: null,
+    },
+  ];
+
+  constructor(questionType: ValueOf<typeof QuestionTypes>) {
+    this.questionType = questionType;
+
+    switch (questionType) {
+      case QuestionTypes.likert:
+        this.initializeLikert();
+        break;
+      case QuestionTypes.shortAnswer:
+        this.initializeShortAnswer();
+        break;
+      case QuestionTypes.singleSelect:
+        this.initializeSingleSelect();
+        break;
+      case QuestionTypes.multiSelect:
+        this.initializeMultiSelect();
+        break;
+      case QuestionTypes.extent:
+        this.initializeExtent();
+        break;
+      case QuestionTypes.longAnswer:
+        this.initializeLongAnswer();
+        break;
+    }
+  }
+
+  initializeLikert() {
+    this.commonChoices = [
+      {
+        number: 1,
+        content: '매우 그렇다',
+        isDescriptive: false,
+        descForm: null,
+      },
+      {
+        number: 2,
+        content: '그렇다',
+        isDescriptive: false,
+        descForm: null,
+      },
+      {
+        number: 3,
+        content: '보통이다',
+        isDescriptive: false,
+        descForm: null,
+      },
+      {
+        number: 4,
+        content: '그렇지 않다',
+        isDescriptive: false,
+        descForm: null,
+      },
+      {
+        number: 5,
+        content: '매우 그렇지 않다',
+        isDescriptive: false,
+        descForm: null,
+      },
+    ];
+  }
+
+  initializeShortAnswer() {
+    this.questions[0].choices = [
+      {
+        number: 1,
+        content: '선택1',
+        isDescriptive: true,
+        descForm: '%d',
+      },
+    ];
+  }
+
+  initializeSingleSelect() {
+    this.questions[0].choices = [
+      {
+        number: 1,
+        content: '선택1',
+        isDescriptive: false,
+        descForm: null,
+      },
+    ];
+  }
+
+  initializeMultiSelect() {
+    this.questions[0].choices = [
+      {
+        number: 1,
+        content: '선택1',
+        isDescriptive: false,
+        descForm: null,
+      },
+    ];
+  }
+
+  initializeExtent() {
+    this.commonChoices = [
+      {
+        number: 1,
+        content: '낮음',
+        isDescriptive: false,
+        descForm: null,
+      },
+      {
+        number: 2,
+        content: '높음',
+        isDescriptive: false,
+        descForm: null,
+      },
+    ];
+    this.questions = [
+      ...this.questions,
+      {
+        number: 2,
+        content: '질문2',
+        isRequired: true,
+        linkedSector: null,
+        choices: null,
+      },
+    ];
+  }
+
+  // eslint-disable-next-line no-restricted-syntax, @typescript-eslint/no-empty-function
+  initializeLongAnswer() {}
+}
