b10cc43d645a47458d600719671e9e2250a8c8e8
sookyeongyeom <sookyeong.dev@gmail.com>
2023-03-06 01:10:47 +0900

    Feat: apply RouterGuard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,12 @@ import { useAtomsDevtools } from 'jotai-devtools';
 import { ReactElement } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { GlobalStyle } from '../styles/global';
+import { store } from '@atoms';
+import { RouterGuard } from '@components';
+import { GlobalStyle } from '@styles';
 
 const queryClient = new QueryClient();
+
 const AtomsDevtools = ({ children }: ChildrenType) => {
   useAtomsDevtools('store');
   return children;
@@ -17,12 +20,14 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
   return (
     <AtomsDevtools>
-      <Provider>
-        <QueryClientProvider client={queryClient}>
-          <ReactQueryDevtools initialIsOpen={false} />
-          <Global styles={GlobalStyle} />
-          {getLayout(<Component {...pageProps} />)}
-        </QueryClientProvider>
+      <Provider store={store}>
+        <RouterGuard>
+          <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <Global styles={GlobalStyle} />
+            {getLayout(<Component {...pageProps} />)}
+          </QueryClientProvider>
+        </RouterGuard>
       </Provider>
     </AtomsDevtools>
   );
