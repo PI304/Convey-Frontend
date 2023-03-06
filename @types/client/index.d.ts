355dfef8ce7539978d31348133f9846bbea7585c
sookyeongyeom <sookyeong.dev@gmail.com>
2023-03-07 06:44:46 +0900

    Feat: types for surveysAtom

--- @types/client/index.d.ts
+++ @types/client/index.d.ts
@@ -1,5 +1,22 @@
 import { SerializedStyles } from '@emotion/utils/types';
+import { QuestionTypes } from '@constants';
 
 type LayoutProps = ChildrenType & {
   forwardCss?: SerializedStyles;
+};
+
+type AddQuestionAtomType = {
+  surveyIdx: number;
+  questionType: ValueOf<typeof QuestionTypes>;
+};
+
+type AddChoiceAtomType = {
+  surveyIdx: number;
+  questionIdx: number;
+  questionType: ValueOf<typeof QuestionTypes>;
+};
+
+type AddCommonChoiceAtomType = {
+  surveyIdx: number;
+  questionType: ValueOf<typeof QuestionTypes>;
 };
