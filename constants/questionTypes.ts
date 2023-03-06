e885f13fda979170d2418d3b4216b89efb5e3f89
sookyeongyeom <sookyeong.dev@gmail.com>
2023-03-07 06:45:55 +0900

    Feat: QuestionTypeLabels & QuestionTypeCategories as constants

--- constants/questionTypes.ts
+++ constants/questionTypes.ts
@@ -5,4 +5,17 @@ export const QuestionTypes = {
   multiSelect: 'multi_select',
   extent: 'extent',
   longAnswer: 'long_answer',
-};
+} as const;
+
+export const QuestionTypeLables = {
+  likert: '리커트척도',
+  short_answer: '단답형',
+  single_select: '단일선택',
+  multi_select: '복수선택',
+  extent: '정도',
+  long_answer: '장문형',
+} as const;
+
+export const QuestionTypeCategories = {
+  needCommonChoice: [QuestionTypes.likert, QuestionTypes.extent],
+} as const;
