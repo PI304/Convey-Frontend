fa0c759472bd867a4c3d23053845bb1a0605b3b8
sookyeongyeom <sookyeong.dev@gmail.com>
2023-03-14 03:21:50 +0900

    Feat: AutoResizeTextArea

---
+++ src/components/shared/AutoResizeTextArea.tsx
@@ -0,0 +1,27 @@
+import { useRef, MutableRefObject, useEffect } from 'react';
+import { C } from '@styles';
+
+export const AutoResizeTextArea = ({ value, onChange, placeholder, rows }: AutoResizeTextAreaProps) => {
+  const textAreaRef = useRef() as MutableRefObject<HTMLTextAreaElement>;
+
+  const autoResize = () => {
+    textAreaRef.current.style.height = 'auto';
+    const height = textAreaRef.current.scrollHeight;
+    textAreaRef.current.style.height = height / 10 + 'rem';
+  };
+
+  useEffect(() => {
+    autoResize();
+  }, [value]);
+
+  return (
+    <textarea
+      value={value}
+      onChange={onChange as any}
+      placeholder={placeholder}
+      ref={textAreaRef}
+      rows={rows || 1}
+      css={C.form}
+    />
+  );
+};
