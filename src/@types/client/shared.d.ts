ae9051a4c051a1542a303379dbf40dc17d874ca3
sookyeongyeom <sookyeong.dev@gmail.com>
2023-03-14 02:04:41 +0900

    Feat: ResponseAuthorType & ResponseContactType & IncludedSurveyType

--- src/@types/client/shared.d.ts
+++ src/@types/client/shared.d.ts
@@ -1,6 +1,6 @@
 type CommonButtonProps = {
   label: string;
-  onClick: () => void;
+  onClick: (...args: any) => void;
   backgroundColor?: string;
 };
 
@@ -20,6 +20,7 @@ type InputProps = {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder: string;
   width?: string;
+  disabled?: boolean;
 };
 
 type ModalProps = ChildrenType & {
@@ -27,4 +28,41 @@ type ModalProps = ChildrenType & {
   onCancel: () => void;
   onSubmit: () => void;
   isHidden: boolean;
+};
+
+type PackageBoxProps = {
+  _package: ResponsePackages.Package;
+};
+
+type PartsBoxProps = {
+  packageId: number;
+};
+
+type PartBoxProps = {
+  part: ResponseParts.Part;
+};
+
+type SubjectsBoxProps = {
+  partId: number;
+};
+
+type SubjectBoxProps = {
+  subject: ResponseSubjects.Subject;
+};
+
+type IncludedSurveysBoxProps = {
+  isEditMode: boolean;
+  subjectId: number;
+  onInitIncludedSurveys: (surveys: IncludedSurveyType[]) => void;
+  includedSurveys: IncludedSurveyType[];
+  onChangeNumber: (surveyIdx: number, number: number) => void;
+  onChangeTitle: (surveyIdx: number, title: string) => void;
+  onChangeSurvey: (surveyIdx: number, survey: number) => void;
+  onRemoveSurvey: (surveyIdx: number) => void;
+};
+
+type SelectSurveyDropDownProps = {
+  selectedSurvey: number;
+  onSelect: (surveyId: number) => void;
+  disabled: boolean;
 };
