d9fbd1c6f2751443a1bf642359ed91cda4a66eca
sookyeongyeom <sookyeong.dev@gmail.com>
2023-03-14 17:23:11 +0900

    Fix: useInput onChangeData to setData

--- src/hooks/useInputs.ts
+++ src/hooks/useInputs.ts
@@ -1,13 +1,21 @@
 import produce from 'immer';
 import { useState } from 'react';
 
-export const useInputs = <T>(initialState: T): [T, (e: React.ChangeEvent<HTMLInputElement>, key: keyof T) => void] => {
+export const useInputs = <T>(
+  initialState: T,
+): [T, (e: React.ChangeEvent<HTMLInputElement>, key: keyof T) => void, (data: T) => void] => {
   const [data, setData] = useState<T>(initialState);
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>, key: keyof T) => {
     const newData = produce(data, (draft: any) => {
       if (draft) draft[key] = e.target.value;
     });
     setData(newData);
   };
-  return [data, onChange];
+
+  const onSetData = (data: T) => {
+    setData(data);
+  };
+
+  return [data, onChange, onSetData];
 };
