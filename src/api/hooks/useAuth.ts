1dcf89c9b91547d9e1e0eb207b11b7f804cfd4cd
sookyeongyeom <sookyeong.dev@gmail.com>
2023-03-17 21:29:40 +0900

    Refactor: apply useAuth

--- src/api/hooks/useAuth.ts
+++ src/api/hooks/useAuth.ts
@@ -7,7 +7,7 @@ import { StorageKeys } from '@constants';
 export const useAuth = () => {
   const setAuth = useSetAtom(authAtom);
 
-  const _postLogin = useMutation((data: RequestAuth.PostLogin) => postLogin(data), {
+  const _postLogin = useMutation((params: Parameters<typeof postLogin>) => postLogin(...params), {
     onSuccess: (data) => {
       setAuth({ accessToken: data.accessToken });
       sessionStorage.setItem(StorageKeys.accessToken, data.accessToken);
--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,29 +1,19 @@
 import { css } from '@emotion/react';
-import { useSetAtom } from 'jotai';
-import { useMutation } from 'react-query';
-import { postLogin } from '@api';
-import { authAtom } from '@atoms';
+import { useAuth } from '@api';
 import { Button } from '@components';
-import { StorageKeys } from '@constants';
 import { useInput } from '@hooks/useInput';
 
 export const LoginPage = () => {
-  const setAuth = useSetAtom(authAtom);
+  const { _postLogin } = useAuth();
   const [email, onChangeEmail] = useInput('example@email.com');
   const [password, onChangePassword] = useInput('12345678');
-  const { mutate } = useMutation(() => postLogin({ email, password }), {
-    onSuccess: (data) => {
-      setAuth({ accessToken: data.accessToken });
-      sessionStorage.setItem(StorageKeys.accessToken, data.accessToken);
-    },
-  });
 
   return (
     <div css={Container}>
       <h1>LoginPage</h1>
       <input value={email} onChange={onChangeEmail} placeholder='ID' />
       <input value={password} onChange={onChangePassword} placeholder='비밀번호' />
-      <Button label='로그인' onClick={mutate} />
+      <Button label='로그인' onClick={() => _postLogin.mutate([{ email, password }])} />
     </div>
   );
 };
